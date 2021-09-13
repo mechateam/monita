9c9e738829b24bbcc35ecb1f0940cd5faa1fe5c1
Meldi <meldi.sayoko@pinhome.id>
2021-09-13 22:18:06 +0700

    Initial Commit

---
+++ src/main/java/ta/simonitadepan/monitadepan/MonitaDepanApplication.java
@@ -0,0 +1,13 @@
+package ta.simonitadepan.monitadepan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitaDepanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitaDepanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ta/simonitadepan/monitadepan/MonitaDepanApplicationTests.java
@@ -0,0 +1,13 @@
+package ta.simonitadepan.monitadepan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitaDepanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
