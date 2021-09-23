336825a060e6e5a2fc493ae73c9411e7c4ad8a45
Meldi <meldi.sayoko@pinhome.id>
2021-09-23 14:17:07 +0700

    refactor: encrypt password

--- src/main/java/ta/simonitadepan/monitadepan/service/UserServiceImpl.java
+++ src/main/java/ta/simonitadepan/monitadepan/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package ta.simonitadepan.monitadepan.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import ta.simonitadepan.monitadepan.model.UserModel;
 import ta.simonitadepan.monitadepan.repository.UserDb;
@@ -15,5 +16,16 @@ public class UserServiceImpl implements UserService {
     UserDb userDb;
 
     @Override
-    public void addUser(UserModel user) {userDb.save(user);}
+    public void addUser(UserModel user) {
+        String pass = encrypt(user.getPassword());
+        user.setPassword(pass);
+        userDb.save(user);
+    }
+
+    @Override
+    public String encrypt(String password){
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String hashedPassword = passwordEncoder.encode(password);
+        return hashedPassword;
+    }
 }
