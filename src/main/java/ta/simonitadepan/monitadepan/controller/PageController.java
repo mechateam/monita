c7be377dfd755257343d2fc28e056b2288af877a
Meldi <meldi.sayoko@pinhome.id>
2021-10-21 14:05:50 +0700

    feat: home screen

--- src/main/java/ta/simonitadepan/monitadepan/controller/PageController.java
+++ src/main/java/ta/simonitadepan/monitadepan/controller/PageController.java
@@ -1,15 +1,62 @@
 package ta.simonitadepan.monitadepan.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ta.simonitadepan.monitadepan.model.BalitaModel;
+import ta.simonitadepan.monitadepan.model.PertumbuhanBalitaModel;
+import ta.simonitadepan.monitadepan.service.BalitaService;
+import ta.simonitadepan.monitadepan.service.PertumbuhanService;
+import ta.simonitadepan.monitadepan.service.UserService;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class PageController {
 
+    @Autowired
+    BalitaService balitaService;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    PertumbuhanService pertumbuhanService;
+
     @GetMapping("/")
-    public String landingPage(){
+    public String landingPage(Model model){
+
+        BalitaModel balita = balitaService.getBalitaAktif(userService.getUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName()));
+
+        PertumbuhanBalitaModel pertumbuhanBalitaModel  = pertumbuhanService.getPertumbuhanBulanIni(balita.getListPertumbuhan());
+        Map<String, Integer> umur = balitaService.calculateAge(balita.getBirth_date());
+
+        if (pertumbuhanBalitaModel == null){
+            model.addAttribute("isi",false);
+        }
+        else{
+            model.addAttribute("isi",true);
+        }
+
+        if (pertumbuhanService.getHasilDiagnosisBulanIni(pertumbuhanBalitaModel.getDiagnosis())){
+            model.addAttribute("diagnosis", "SESUAI");
+        }
+        else{
+            model.addAttribute("diagnosis", "PERHATIAN");
+        }
+
+        model.addAttribute("berat", pertumbuhanBalitaModel.getBerat_badan());
+        model.addAttribute("tinggi", pertumbuhanBalitaModel.getTinggi_badan());
+        model.addAttribute("balita",balita);
+        model.addAttribute("bulan",umur.get("bulan"));
+        model.addAttribute("tahun",umur.get("tahun"));
         return "page-home";
     }
 
--- src/main/java/ta/simonitadepan/monitadepan/repository/PertumbuhanDb.java
+++ src/main/java/ta/simonitadepan/monitadepan/repository/PertumbuhanDb.java
@@ -3,5 +3,7 @@ package ta.simonitadepan.monitadepan.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ta.simonitadepan.monitadepan.model.PertumbuhanBalitaModel;
 
+import java.util.Date;
+
 public interface PertumbuhanDb extends JpaRepository<PertumbuhanBalitaModel, Long> {
 }
