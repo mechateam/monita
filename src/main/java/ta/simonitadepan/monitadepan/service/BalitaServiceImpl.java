1172812e88a73f3d0b3bf858fae7730fe7706b58
Meldi Hafizh <51785761+mechateam@users.noreply.github.com>
2021-11-17 22:27:32 +0700

    fix: fix delete balita

--- src/main/java/ta/simonitadepan/monitadepan/service/BalitaServiceImpl.java
+++ src/main/java/ta/simonitadepan/monitadepan/service/BalitaServiceImpl.java
@@ -72,6 +72,7 @@ public class BalitaServiceImpl implements BalitaService {
     public void deleteBalita(BalitaModel balita) {
         UserModel user = balita.getId_pengguna();
         balitaDb.delete(balita);
+        balitaDb.save(balita);
         if (user.getListBalita().size() !=0 && user.getListBalita()!= null){
             user.getListBalita().get(0).setStatus(1);
             balitaDb.save(user.getListBalita().get(0));
